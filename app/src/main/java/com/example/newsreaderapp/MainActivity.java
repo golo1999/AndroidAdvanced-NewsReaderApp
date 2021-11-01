b95161c7066b59ed729ba4ce251564a7e1728e64
George-Alexandru Gologan <aggologan1999@gmail.com>
2021-11-01 21:20:56 +0200

    Adding the starting project

---
+++ app/src/main/java/com/example/newsreaderapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.newsreaderapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.newsreaderapp.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
